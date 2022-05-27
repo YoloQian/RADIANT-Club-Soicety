102f0f0d0428ba9fb416c5d90dfaebd75566e5cb
Kingston <wong.brandon111@gmail.com>
2022-05-27 15:52:18 +0800

    yw

---
+++ logic.php
@@ -0,0 +1,12 @@
+<?php
+
+    $query = "SELECT * FROM clubs";
+    $result = mysqli_query($conn,$query);
+
+    if(isset($_REQUEST['cid'])){
+      $cid = $_REQUEST['cid'];
+      $query = "SELECT * FROM clubs WHERE cid = $cid";
+      $result = mysqli_query($conn,$query);
+    }
+
+?>
