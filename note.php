0fc7277c1397961d0994c459e9ddf67eb4e9d41c
Qian <leegamer@gmail.com>
2022-05-27 18:57:49 +0800

    minor

--- note.php
+++
@@ -1,9 +0,0 @@
-<li><a class="dropdown-item" href="adashboard.php"><i class="fa fa-address-card-o" aria-hidden="true"></i>&nbsp;Admin</a></li>
-"<li><a class='dropdown-item' href='adashboard.php'><i class='fa fa-address-card-o' aria-hidden='true'></i>&nbsp;Admin</a></li>"
-
-
-<?php
-if($_SESSION['fullname'] == 'admin'){
-echo "<li><a class='dropdown-item' href='adashboard.php'><i class='fa fa-address-card-o' aria-hidden='true'></i>&nbsp;Admin</a></li>";
-}
-?>
