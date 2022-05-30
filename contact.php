b69e350fe8c0610ff2deddb98aa2d3eb2c384ec5
Tee Chor Yang <cyang0330@gmail.com>
2022-05-31 02:02:00 +0800

    .

--- contact.php
+++ contact.php
@@ -202,7 +202,7 @@
                     <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z"/>
                   </svg>
                     <br><br>
-                    <a href="https://www.facebook.com/Radiant-Club-Soicety-113966541324963" style="text-align:center; font-size: 1.1rem; " class="lh-1">@Radiant Club & Soicety
+                    <a href="https://www.facebook.com/Radiant-Club-Soicety-113966541324963"target="_blank" style="text-align:center; font-size: 1.1rem; " class="lh-1">@Radiant Club & Soicety
                     </a>
                     <br><br><br>
                 </div>
