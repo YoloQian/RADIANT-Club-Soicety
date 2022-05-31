66177765a8bdc055471c385d6fe997523370f0eb
Tee Chor Yang <cyang0330@gmail.com>
2022-06-01 02:00:00 +0800

    .

--- organizer.php
+++ organizer.php
@@ -98,7 +98,6 @@
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Event</h1>
         <div class="btn-toolbar mb-2 mb-md-0"><!--below here-->
-        <button type="btn" class="btn btn-sm btn-outline-primary" onclick="location.href='postevent.php'">Post Event</button>
         <div class="btn-group me-2">
           </div>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar" aria-hidden="true"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
@@ -110,6 +109,8 @@
 
       <!-- Event table -->
       <div class="table-responsive">
+      <button type="btn" class="btn btn-md btn-outline-primary" onclick="location.href='postevent.php'">Post Event</button>
+      <br><br>
         <!--Table list from database for Events-->
         <?php       
         $result =mysqli_query($conn,"SELECT * from events ORDER BY eid DESC");
